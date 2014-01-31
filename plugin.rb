a38b6ba3171057f52608946123b710911931a5c2
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-02-01 04:06:26 +0530

    Revert "Add temporary workaround to fix broken oneboxes."
    This reverts commit 8059bd7800fd1bdce9b4a431204be031fa2e756e.

--- plugin.rb
+++ plugin.rb
@@ -25,8 +25,3 @@ register_asset "javascripts/discourse/templates/post.js.handlebars"
 register_asset "javascripts/discourse/templates/embedded_post.js.handlebars"
 register_asset "javascripts/discourse/templates/header.js.handlebars"
 register_asset "javascripts/discourse/templates/user/user.js.handlebars"
-
-### Temporary: Onebox fix.
-require 'cgi/util'
-CGI::TABLE_FOR_ESCAPE_HTML__ = CGI::Util::TABLE_FOR_ESCAPE_HTML__
-load File.expand_path('../image_onebox.rb', __FILE__)
