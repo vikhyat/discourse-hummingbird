a61fa41089b22cbd5b5c7e03b56be9cf35b1b73a
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-01-31 21:17:49 +0530

    Drop old onebox code.

--- onebox.rb
+++ onebox.rb
@@ -35,27 +35,3 @@ module Onebox
     end
   end
 end
-
-#require 'oneboxer'
-#
-#Oneboxer::Whitelist.entries << Oneboxer::Whitelist::Entry.new(/https?:\/\/hummingbird\.me\/anime\/.+/)
-#
-#class Oneboxer::HummingbirdAnimeOnebox < Oneboxer::HandlebarsOnebox
-#  matcher /https?:\/\/hummingbird\.me\/anime\/.+/
-#
-#  def template
-#    template_path('simple_onebox')
-#  end
-#
-#  def onebox
-#    slug = @url.match(/https?:\/\/hummingbird\.me\/anime\/(.+)/)[1]
-#    anime_json = open("http://hummingbird.me/api/v1/anime/#{slug}").read
-#    anime = JSON.parse anime_json
-#
-#    result = {}
-#
-#    Mustache.render(File.read(template), result)
-#  end
-#end
-#
-#Oneboxer.add_onebox Oneboxer::HummingbirdAnimeOnebox
