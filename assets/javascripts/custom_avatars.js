95c298852cf7bd699be231709a9ca91e26e004e6
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-03-08 21:45:31 +0530

    Fix missing small avatars.

--- assets/javascripts/custom_avatars.js
+++ assets/javascripts/custom_avatars.js
@@ -1,7 +1,8 @@
 Discourse.Utilities.avatarUrl = function(template, size) {
   if (!template) { return ""; }
   if (size <= 25) {
-    return template.replace(/\{size\}/g, 'small');
+    return template.replace(/\.[a-z]+\?/, '.jpg?')
+                   .replace(/\{size\}/g, 'small');
   }
   else {
     return template.replace(/\{size\}/g, 'thumb');
